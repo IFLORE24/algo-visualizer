5d86734efd521f2291501dc0ef9c71acfd755a5b
davi-maciel <70175298+davi-maciel@users.noreply.github.com>
2025-08-23 18:51:53 -0500

    Add buttons

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -3,36 +3,56 @@ import './SortingVisualizer.css';
 
 export default class SortingVisualizer extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {array: [], };
+        this.state = { array: [], };
     }
-        
-    componentDidMount(){
+
+    componentDidMount() {
         this.resetArray();
     }
 
-    resetArray(){
+    resetArray() {
         const array = [];
-        for(let i=0; i<100; i++){
+        for (let i = 0; i < 100; i++) {
             array.push(randomIntFromInterval(5, 750));
         }
-        this.setState({array});
+        this.setState({ array });
     }
 
-    render(){
-        const {array} = this.state;
-        return (<div className="array-container"> 
-        
-        {array.map((value, idx) => (
-            <div className="array-bar" key={idx}
-                style = {{height:`${value}px`}}>
-            </div>))} 
-        </div>);
+    mergeSort() {
+
     }
-        
+
+    quickSort() { }
+
+    render() {
+        const { array } = this.state;
+
+        return (
+            <div>
+                <div className="array-container">
+                    {array.map((value, idx) => (
+                        <div
+                            className="array-bar"
+                            key={idx}
+                            style={{ height: `${value}px` }}>
+                        </div>
+                    ))}
+                </div>
+                <div className="button-container">
+                    <button onClick={() => this.resetArray()}>
+                        Generate New Array
+                    </button>
+                    <button onClick={() => this.mergeSort()}>Merge Sort</button>
+                    <button onClick={() => this.quickSort()}>Quick Sort</button>
+                </div>
+            </div>
+        );
+    }
+
 
 }
-    function randomIntFromInterval(min, max){ //this finds a random number between min and max
-        return Math.floor(Math.random()*(max-min+1)+min) 
-    }
+function randomIntFromInterval(min, max) { //this finds a random number between min and max
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
