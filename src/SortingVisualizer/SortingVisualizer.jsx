f13f174f537e2b9bd43d0752f0cec9c18e76f751
davi-maciel <70175298+davi-maciel@users.noreply.github.com>
2025-08-26 22:04:32 -0500

    Add merge sort
    Co-authored-by: IFLORE24 <IFLORE24@users.noreply.github.com>

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { getMergeSortAnimations } from "../SortingAlgorithms/SortingAlgorithms";
 import './SortingVisualizer.css';
+
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 1;
+
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 100;
+
+// This is the main color of the array bars.
+const PRIMARY_COLOR = 'turquoise';
+
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = 'red';
 
 export default class SortingVisualizer extends React.Component {
 
@@ -14,14 +27,34 @@ export default class SortingVisualizer extends React.Component {
 
     resetArray() {
         const array = [];
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
             array.push(randomIntFromInterval(5, 750));
         }
         this.setState({ array });
     }
 
     mergeSort() {
-
+        const animations = getMergeSortAnimations(this.state.array);
+        for (let i = 0; i < animations.length; i++) {
+            const arrayBars = document.getElementsByClassName('array-bar');
+            const isColorChange = i % 3 !== 2;
+            if (isColorChange) {
+                const [barOneIdx, barTwoIdx] = animations[i];
+                const barOneStyle = arrayBars[barOneIdx].style;
+                const barTwoStyle = arrayBars[barTwoIdx].style;
+                const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color;
+                    barTwoStyle.backgroundColor = color;
+                }, i * ANIMATION_SPEED_MS);
+            } else {
+                setTimeout(() => {
+                    const [barOneIdx, newHeight] = animations[i];
+                    const barOneStyle = arrayBars[barOneIdx].style;
+                    barOneStyle.height = `${newHeight}px`;
+                }, i * ANIMATION_SPEED_MS);
+            }
+        }
     }
 
     quickSort() { }
@@ -30,23 +63,21 @@ export default class SortingVisualizer extends React.Component {
         const { array } = this.state;
 
         return (
-            <div>
-                <div className="array-container">
-                    {array.map((value, idx) => (
-                        <div
-                            className="array-bar"
-                            key={idx}
-                            style={{ height: `${value}px` }}>
-                        </div>
-                    ))}
-                </div>
-                <div className="button-container">
-                    <button onClick={() => this.resetArray()}>
-                        Generate New Array
-                    </button>
-                    <button onClick={() => this.mergeSort()}>Merge Sort</button>
-                    <button onClick={() => this.quickSort()}>Quick Sort</button>
-                </div>
+            <div className="array-container">
+                {array.map((value, idx) => (
+                    <div
+                        className="array-bar"
+                        key={idx}
+                        style={{
+                            backgroundColor: PRIMARY_COLOR,
+                            height: `${value}px`,
+                        }}></div>
+                ))}
+                <button onClick={() => this.resetArray()}>Generate New Array</button>
+                <button onClick={() => this.mergeSort()}>Merge Sort</button>
+                <button onClick={() => this.quickSort()}>Quick Sort</button>
+                <button onClick={() => this.heapSort()}>Heap Sort</button>
+                <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
             </div>
         );
     }
